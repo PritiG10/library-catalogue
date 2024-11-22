cee40c8dc1d9bce7654086d6ccba7b595d23f818
Priti Gabhane <108253701+PritiG10@users.noreply.github.com>
2024-11-22 12:56:58 +0530

    Update catalogue.h

--- catalogue.h
+++ catalogue.h
@@ -1,15 +1,5 @@
 #include "book.h"
 
-// Array-based implementation of LibraryCatalogue
-typedef struct {
-    struct Book* books;         // Dynamic array of books
-    int size;            // Current number of books
-    int capacity;        // Maximum capacity of the array
-} LibraryCatalogue;
-
-// Function prototypes
-LibraryCatalogue* initializeCatalogue(int initial_capacity);
-void addBook(LibraryCatalogue* catalogue, struct Book book);
-void displayCatalogue(const LibraryCatalogue* catalogue);
-void freeCatalogue(LibraryCatalogue* catalogue);
-
+struct LibraryCatalogue{
+    struct Book* NEXT;    
+};
